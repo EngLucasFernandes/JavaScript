bb45f31cc3868513300c6f179c47f6ff310592ea
Lucas Fernandes <100652686+EngLucasFernandes@users.noreply.github.com>
2022-08-02 14:38:33 -0300

    Add files via upload

---
+++ JavaScript/Boolean.js
@@ -0,0 +1,18 @@
+// Boolean
+
+console.log(5 > 3)  // True
+console.log(42 < 10)  // False
+
+console.log(typeof true)
+console.log(typeof false)
+
+// Operadores Relacionais (>, <, >=, <=, ==, ===, !=, !==)
+
+console.log(10 > 2)
+console.log(7 < 3)
+console.log(5 >= 5)
+console.log(1 <= 2)
+console.log(10 == '10')  // True
+console.log(10 === '10')  // False
+
+// Operadores Lógicos
---
+++ JavaScript/Number.js
@@ -0,0 +1,27 @@
+// Number
+
+console.log(typeof 13)
+console.log(typeof 1.8)
+console.log(typeof -5)
+
+// Operações Aritméticas
+
+console.log(2 + 4)  // + -> Adição
+console.log(5 - 3)  // - -> Subtração
+console.log(5 * 3)  // * -> Multiplicação
+console.log(10 / 2)  // / -> Divisão
+console.log(10 % 2)  // % -> Módulo 
+
+// Special Numbers (Infinity, -Infinity, NaN)
+
+console.log(Infinity)
+console.log(-Infinity)
+console.log(NaN)
+
+console.log(typeof Infinity)  // Number
+console.log(typeof -Infinity)  // Number
+console.log(typeof NaN)  // Number
+
+console.log(-10 / 0)  // -Infinity
+console.log(5 / 0)  // Infinity
+console.log('JavaScript' / 10)  // NaN
