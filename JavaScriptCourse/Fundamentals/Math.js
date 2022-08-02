077f289d55a5b2d98f01cbd23f5892f896671f77
Lucas Fernandes <100652686+EngLucasFernandes@users.noreply.github.com>
2022-08-02 14:38:52 -0300

    Delete JavaScriptCourse/Fundamentals directory

--- JavaScriptCourse/Fundamentals/Math.js
+++
@@ -1,4 +0,0 @@
-const radius = 5.6
-const area = Math.PI * radius * radius
-
-console.log(area.toFixed(2))
--- JavaScriptCourse/Fundamentals/WeakTyping.js
+++
@@ -1,9 +0,0 @@
-// Em JavaScript, por causa da tipagem fraca, não é possível declarar uma variável que armazene um determinado tipo de dado, por isso as variáveis podem armazenar um tipo de dado em um momento e depois armazenar outro tipo de dado.
-
-let language = "JavaScript"
-console.log(language)
-console.log(typeof language)  // Tipo da variável 'language' no momento
-
-language = 10
-console.log(language)
-console.log(typeof language)
